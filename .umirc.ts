d43e31f3f0b36e5951fe0412ab3bd9585d7092a6
junshun.mq <junshun.mq@alibaba-inc.com>
2021-01-19 22:51:45 +0800

    feat: 部署配置

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
+  base: '/ganfan',
+  publicPath: '/ganfan/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+  // 其他配置
   // more config: https://d.umijs.org/config
 });
