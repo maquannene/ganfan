0b8e76f950eca6bae1b9e3f8a737ea20b27bcd67
junshun.mq <junshun.mq@alibaba-inc.com>
2023-02-23 17:37:13 +0800

    feat: 修改 base

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: '干（gàn）饭食堂',
   mode: 'site',
-  base: '/ganfan',
+  base: '/',
   publicPath: '/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // 其他配置
