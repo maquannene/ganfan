d7dd743a51db62fde4cefa7ea36be282e20aae73
junshun.mq <junshun.mq@alibaba-inc.com>
2023-02-23 17:25:39 +0800

    fix: publicPath 修改

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   title: '干（gàn）饭食堂',
   mode: 'site',
   base: '/ganfan',
-  publicPath: '/ganfan/',
+  publicPath: '/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // 其他配置
   // more config: https://d.umijs.org/config
